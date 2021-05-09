c3f12aa794ecda0b7ff8f3ba713744f65cd68ee9
jojapoppa <jojapoppa@protonmail.com>
2021-05-09 10:58:10 -0700

    updated link

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -306,7 +306,7 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
             <ButtonSecondary padding="8px" borderRadius="8px">
               <ExternalLink
                 style={{ width: '100%', textAlign: 'center' }}
-                href={`https://uniswap.info/account/${account}`}
+                href={`https://v2.info.uniswap.org/account/${account}`}
               >
                 View accrued fees and analytics<span style={{ fontSize: '11px' }}>↗</span>
               </ExternalLink>
